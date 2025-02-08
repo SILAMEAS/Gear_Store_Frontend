d1891f906faff59e5da2ed7bbdc6a06b26b05e95
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-02-08 11:47:11 +0700

    remove-clerk

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,14 @@
-import { Provider } from "react-redux";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
+import {Provider} from "react-redux";
+import {createRoot} from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import { store } from "./redux.ts";
-
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
+import {store} from "./redux.ts";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
-      </ClerkProvider>
     </Provider>
   </BrowserRouter>,
 );
