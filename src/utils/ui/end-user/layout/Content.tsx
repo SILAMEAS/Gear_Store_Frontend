338f75b4023b12cb6808e1b1f5fc064c069f3960
Sila MEAS <sila.meas@allweb.com.kh>
2025-03-10 17:54:50 +0700

    layout-login-page

--- src/utils/ui/end-user/layout/Content.tsx
+++ src/utils/ui/end-user/layout/Content.tsx
@@ -1,8 +1,12 @@
 import {Outlet} from "react-router-dom";
 import {Stack} from "@mui/material";
+import useCheckUrl from "../../../hooks/useCheckUrl.tsx";
 
-const AppContent = () => <Stack height={"calc(100vh - 140px)"} overflow={"hidden"} bgcolor={"black"}>
-    <Outlet />
-</Stack>
+const AppContent = () => {
+    const {IsLoginPage}=useCheckUrl();
+    return <Stack height={`calc(100vh - ${IsLoginPage?40:140}px)`} overflow={"hidden"} bgcolor={"black"}>
+        <Outlet />
+    </Stack>
+}
 
 export default AppContent;
