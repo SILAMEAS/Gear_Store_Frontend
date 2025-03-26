bb1ce57380d652de6c90ae6138307d964cd79fab
Sila MEAS <sila.meas@allweb.com.kh>
2025-03-26 17:45:55 +0700

    susense

--- src/theme/provider/ThemeProvider.tsx
+++ src/theme/provider/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import {createContext, type ReactNode, useCallback, useContext, useEffect, useMemo, useState} from "react";
+import {createContext, type ReactNode, Suspense, useCallback, useContext, useEffect, useMemo, useState} from "react";
 import {ThemeProvider as MUIThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import darkTheme from "../mode/darkTheme.tsx";
@@ -37,11 +37,13 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
     const contextValue = useMemo(() => ({ isDarkMode, toggleTheme ,colorBackWhite}), [isDarkMode,toggleTheme,colorBackWhite]);
 
     return (
-        <ThemeContext.Provider value={contextValue}>
-            <MUIThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-                <CssBaseline />
-                {children}
-            </MUIThemeProvider>
-        </ThemeContext.Provider>
+       <Suspense fallback={<>loading ... </>}>
+           <ThemeContext.Provider value={contextValue}>
+               <MUIThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+                   <CssBaseline />
+                   {children}
+               </MUIThemeProvider>
+           </ThemeContext.Provider>
+       </Suspense>
     );
 }
